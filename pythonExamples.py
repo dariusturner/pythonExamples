aad660bbf14e197f3d4186a60f2359854e2324a4
Darius Turner <dariusot731@gmail.com>
2019-06-04 07:47:53 -0400

    Added Comments and Math Operators

--- pythonExamples.py
+++ pythonExamples.py
@@ -1,1 +1,19 @@
-print('hello world')
+# Print Hello World to console
+print('Hello World')
+
+# Math Operators
+print(22 - 5)
+
+print(22 - 5)
+
+print(22 * 5)
+
+print(22 / 5)
+
+print(22 % 5)
+
+print(22 ** 5)
+
+print(22 + 5 * 2)
+
+print((22 - 5) + 5) 
