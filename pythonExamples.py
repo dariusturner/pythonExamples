a5ea245fbf3afa19e98b700b85eae7549ac1a19e
Darius Turner <dariusot731@gmail.com>
2019-06-06 19:46:14 -0400

    Completed Statement Test

--- pythonExamples.py
+++ pythonExamples.py
@@ -588,9 +588,9 @@ else:
     print("You can't even drink yet!")'''
 
 
-# List Tips Section
+# List Comprehension Section
 ##############################################################################################
-sectionName = 'List Tips'
+sectionName = 'List Comprehension'
 print(f'{sectionName} Section')
 # Break down the string and add the indexes of the string to your list
 myList = []
@@ -826,30 +826,50 @@ sectionName = "Statments"
 print(f'{sectionName} Test' * 8)
 
 #Use for, .split(), and if to create a Statement that will print out words that start with 's':
-st = 'Print only the words that start with s in this sentence'
 
 #Code here
+st = 'Print only the words that start with s in this sentence'
+st = st.split()
+for word in st:
+    for letter in word:
+        if letter == 's':
+            print(word)
 
 #Use range() to print all the even numbers from 0 to 10.
 
 #Code Here
+for num in range(11):
+    print(num)
 
 #Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
 
 #Code here
+[print(num) for num in range(1,51) if num % 3 == 0]
 
 #Go through the string below and if the length of a word is even print "even!"
 
-st = 'Print every word in this sentence that has an even number of letters'
-
 #Code here
+st = 'Print every word in this sentence that has an even number of letters'
+for word in st.split():
+    if len(word) % 2 == 0:
+        print('even!')
 
 #Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
 #Code here
+for num in range(1,101):
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 5 == 0:
+        print('Buzz')
+    elif num % 3 == 0:
+        print('Fizz')
 
 #Use List Comprehension to create a list of the first letters of every word in the string below:
 
 st = 'Create a list of the first letters of every word in this string'
 
 #Code here
+firstLetters = []
+[firstLetters.append(word[0]) for word in st.split(' ') if word[0] == word[0]]
+print(firstLetters)
