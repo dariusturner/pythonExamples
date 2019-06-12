e86d9a91a48272c529cb5e557bc17ec1d58e2f37
Darius Turner <dariusot731@gmail.com>
2019-06-12 09:02:16 -0400

    Simplified makes_twenty function

--- pythonExamples.py
+++ pythonExamples.py
@@ -814,10 +814,8 @@ print(animal_crackers('Crazy Kangaroo'))
 #makes_twenty(2,3) --> False
 
 def makes_twenty(n1,n2):
-    if n1 + n2 == 20 or n1 == 20 or n2 == 20:
-        return True
-    else:
-        return False
+    return (n1 + n2) == 20 or n1 == 20 or n2 == 20
+
 
 # Check
 print(makes_twenty(20,10))
