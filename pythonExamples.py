204a692488d6e328ce63892af8e88e9d64518a47
Darius Turner <dariusot731@gmail.com>
2019-06-12 08:08:15 -0400

    Warm Up Function Exercises Completed

--- pythonExamples.py
+++ pythonExamples.py
@@ -762,6 +762,86 @@ combineArgsFunc(2,5,6,8, meat='wagyu', dish='chicken parmesean')
 
 
 
+
+
+
+
+
+
+
+##########################################################
+#Function Exercises Section
+##########################################################
+sectionName = 'Function Exercises'
+print('{} Section'.format(sectionName))
+
+###### WARM UP #################
+
+#LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers if both numbers are even, but returns the greater if one or both numbers are odd
+#lesser_of_two_evens(2,4) --> 2
+#lesser_of_two_evens(2,5) --> 5
+
+def lesser_of_two_evens(a,b):
+    if a % 2 == 0 and b % 2 == 0:
+        return min(a,b)
+    else:
+        return max(a,b)
+
+
+# Check
+print(lesser_of_two_evens(2,4))
+
+# Check
+print(lesser_of_two_evens(2,5))
+
+#ANIMAL CRACKERS: Write a function takes a two-word string and returns True if both words begin with same letter
+#animal_crackers('Levelheaded Llama') --> True
+#animal_crackers('Crazy Kangaroo') --> False
+
+def animal_crackers(text):
+    words = text.lower().split()
+    return words[0][0] == words[1][0]
+
+# Check
+print(animal_crackers('Levelheaded Llama'))
+
+# Check
+print(animal_crackers('Crazy Kangaroo'))
+
+#MAKES TWENTY: Given two integers, return True if the sum of the integers is 20 or if one of the integers is 20. If not, return False
+#makes_twenty(20,10) --> True
+#makes_twenty(12,8) --> True
+#makes_twenty(2,3) --> False
+
+def makes_twenty(n1,n2):
+    if n1 + n2 == 20 or n1 == 20 or n2 == 20:
+        return True
+    else:
+        return False
+
+# Check
+print(makes_twenty(20,10))
+
+# Check
+print(makes_twenty(2,3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ''' #######################################################################################################################
 # Test # 1
 #######################################################################################################################
@@ -987,3 +1067,4 @@ st = 'Create a list of the first letters of every word in this string'
 firstLetters = []
 [word[0] for word in st.split(' ')]
 print(firstLetters)'''
+
