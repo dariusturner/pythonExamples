e8f0548340704cd26364acbb81c5dd30349bd5fa
Darius Turner <dariusot731@gmail.com>
2019-06-12 21:03:35 -0400

    Level 2 Completed

--- pythonExamples.py
+++ pythonExamples.py
@@ -769,6 +769,29 @@ combineArgsFunc(2,5,6,8, meat='wagyu', dish='chicken parmesean')
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ##########################################################
 #Function Exercises Section
 ##########################################################
@@ -880,6 +903,107 @@ print(almost_there(150))
 
 # Check
 print(almost_there(209))
+
+
+############### Level 2 Problems ###############
+
+#Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
+###has_33([1, 3, 3]) → True
+###has_33([1, 3, 1, 3]) → False
+###has_33([3, 1, 3]) → False
+
+def has_33(nums):
+    for index in range(len(nums) -1):
+        if (nums[index] == 3) and (nums[index + 1] == 3):
+            return True
+    return False
+
+
+# Check
+print(has_33([1, 3, 3]))
+
+# Check
+print(has_33([1, 3, 1, 3]))
+
+# Check
+print(has_33([3, 1, 3]))
+
+#PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
+####aper_doll('Hello') --> 'HHHeeellllllooo'
+####paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii'
+
+def paper_doll(text):
+    answer = ''
+    for letter in text:
+        answer = answer + (letter * 3)
+    return answer
+
+# Check
+print(paper_doll('Hello'))
+
+# Check
+paper_doll('Mississippi')
+
+
+#BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
+####blackjack(5,6,7) --> 18
+####blackjack(9,9,9) --> 'BUST'
+####blackjack(9,9,11) --> 19
+
+def blackjack(a,b,c):
+    sum = a + b + c
+    if sum <= 21:
+        return sum
+    elif sum > 21 and (a == 11 or b == 11 or c == 11):
+        return sum - 10
+    else:
+        return 'BUST'
+
+# Check
+print(blackjack(5,6,7))
+
+# Check
+print(blackjack(9,9,9))
+
+# Check
+print(blackjack(9,9,11))
+
+
+#SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
+###summer_69([1, 3, 5]) --> 9
+###summer_69([4, 5, 6, 7, 8, 9]) --> 9
+###summer_69([2, 1, 6, 9, 11]) --> 14
+
+def summer_69(arr):
+    
+    sum = 0
+    add = True
+    
+    for num in arr:
+        while add:
+            if num != 6:
+                sum += num
+                break
+            else:
+                add = False
+        while not add:
+            if num != 9:
+                break
+            else:
+                add = True
+                break
+    return sum 
+        
+
+# Check
+print(summer_69([1, 3, 5]))
+
+# Check
+print(summer_69([4, 5, 6, 7, 8, 9]))
+
+# Check
+print(summer_69([2, 1, 6, 9, 11]))
+
 
 
 
